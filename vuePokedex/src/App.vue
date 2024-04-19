e42db9ad74e0987bb38d7cab7f6bb3413fff6bad
Stanley <27.stanleylch@gmail.com>
2024-04-19 11:16:23 +0100

    refactored fetch to use props, converted list components into cards

--- vuePokedex/src/App.vue
+++ vuePokedex/src/App.vue
@@ -4,42 +4,45 @@
       <img src="./assets/pokemonLogo.svg" class="logo" alt="PokeLogo" />
     </a>
   </div>
-  <HelloWorld msg="Pokedex" />
-  <div>
-    <button @click="logKantoPokemon">consoleLogKantoPokemon</button>
-  </div>
   <p>Find Pokemon: {{ filterText }}</p>
   <input type="text" v-model="filterText" />
   <div>
     <ul>
-      <li
-        v-for="(pokemon, index) in filteredPokemonList"
+      <PokedexCard
+        v-for="(pokemon, index) in storedPokemon.filteredListOfPokemon"
         :key="`poke-${index}`"
-      >
-        #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }} -
-        {{ pokemon.pokemon_species.url }}
-      </li>
+        :number="pokemon.entry_number"
+        :name="pokemon.pokemon_species.name"
+      />
+      <!-- #{{ pokemon.entry_number }} - {{ pokemon.pokemon_species.name }} -
+        {{ pokemon.pokemon_species.url }} -->
+      <!-- </PokedexCard> -->
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, reactive } from "vue";
+import PokedexCard from "./components/PokedexCard.vue";
 
 const pokemonList = ref([]);
 const filterText = ref("");
-const filteredPokemonList = computed(() => {
-  return pokemonList.value.filter((pokemon) => {
-    return pokemon.pokemon_species.name.includes(filterText.value);
-  });
+
+const storedPokemon = reactive({
+  list: [],
+  filteredListOfPokemon: computed(() =>
+    storedPokemon.list.filter((pokemon) =>
+      pokemon.pokemon_species.name.includes(filterText.value)
+    )
+  ),
 });
 
 onMounted(async () => {
-  const pokeData = await fetch("https://pokeapi.co/api/v2/pokedex/2/")
-    .then((response) => response.json())
-    .then((data) => {
-      pokemonList.value = data.pokemon_entries;
-    });
+  const pokeData = await fetch("https://pokeapi.co/api/v2/pokedex/2/").then(
+    (response) => response.json()
+  );
+
+  storedPokemon.list = pokeData.pokemon_entries;
 });
 </script>
 
---
+++ vuePokedex/src/components/PokedexCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  number: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <li>{{ number }} - {{ name }}</li>
+</template>
+
+<style></style>
